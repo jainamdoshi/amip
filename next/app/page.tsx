a0921b889dd3d5f5aaa122e599508fe88a2d1ce3
Jainam Doshi <jainam.doshi@clinibase.com>
2025-05-11 18:22:28 +1000

    Disable featured products

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <main>
             <Hero />
-            <FeaturedProducts />
+            {/* <FeaturedProducts /> */}
         </main>
     );
 }
